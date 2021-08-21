033f5d33b58035b688b367ddf8ed8b430c93f0d8
Adam Victor <official.LKON@gmail.com>
2021-08-20 21:02:33 -0700

    Double quotes, run prettier

--- index.ts
+++ index.ts
@@ -3,37 +3,37 @@ import * as readline from "readline";
 
 const args = process.argv.slice(2);
 if (args.length > 1) {
-    console.log("Usage: tslox [script]");
-    process.exit(64);
+  console.log("Usage: tslox [script]");
+  process.exit(64);
 } else if (args.length === 1) {
-    runFile(args[0]);
+  runFile(args[0]);
 } else {
-    runPrompt();
+  runPrompt();
 }
 
 function runFile(path: string) {
-    const fileBuffer = fs.readFileSync(path);
-    run(fileBuffer.toString());
+  const fileBuffer = fs.readFileSync(path);
+  run(fileBuffer.toString());
 }
 
 function runPrompt() {
-    const reader = readline.createInterface({
-        input: process.stdin,
-        output: process.stdout
+  const reader = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+  function runNextLine() {
+    reader.question("> ", (line) => {
+      run(line);
+      runNextLine();
     });
-    function runNextLine() {
-        reader.question('> ', line => {
-            run(line);
-            runNextLine();
-        });
-    }
-    runNextLine();
+  }
+  runNextLine();
 }
 
 function run(source: string) {
-    // const scanner = new Scanner();
-    // const tokens = scanner.scanTokens();
-    // for (const aToken of tokens) {
-    //     console.log(aToken);
-    // }
+  // const scanner = new Scanner();
+  // const tokens = scanner.scanTokens();
+  // for (const aToken of tokens) {
+  //     console.log(aToken);
+  // }
 }
